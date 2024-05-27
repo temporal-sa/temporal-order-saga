3b301c582cf1ab52da3db68473f1156a2dfa2648
Steve Androulakis <steve.androulakis@gmail.com>
2024-05-27 12:38:22 -0700

    code commenting

--- core/src/main/java/io/temporal/samples/ordersaga/OrderActivitiesImpl.java
+++ core/src/main/java/io/temporal/samples/ordersaga/OrderActivitiesImpl.java
@@ -23,65 +23,65 @@ import io.temporal.failure.ApplicationFailure;
 
 public class OrderActivitiesImpl implements OrderActivities {
 
-  @Override
-  public void processPayment(String orderId) {
-    System.out.println("Processing payment for order: " + orderId);
-    sleep(5); // simulate API call processing time
-    // Implement the actual payment processing logic here
-  }
-
-  @Override
-  public void reserveInventory(String orderId) {
-    System.out.println("Reserving inventory for order: " + orderId);
-    sleep(5); // simulate API call processing time
-    // Implement the actual inventory reservation logic here
-
-    // Simulate inventory reservation failure
-        Exception e = new RuntimeException("Inventory reservation failed");
-        throw ApplicationFailure.newNonRetryableFailure(e.getMessage(), e.getClass().getName());
-  }
-
-  @Override
-  public void deliverOrder(String orderId) {
-    System.out.println("Delivering order: " + orderId);
-    sleep(5); // simulate API call processing time
-    // Implement the actual order delivery logic here
-
-    // Simulate delivery failure
-    //    Exception e = new RuntimeException("Delivery failed");
-    //    throw ApplicationFailure.newNonRetryableFailure(e.getMessage(), e.getClass().getName());
-  }
-
-  @Override
-  public void refundPayment(String orderId) {
-    sleep(5); // simulate API call processing time
-    System.out.println("Refunding payment for order: " + orderId);
-    // Implement the actual payment refund logic here
-  }
-
-  @Override
-  public void restockInventory(String orderId) {
-    sleep(5); // simulate API call processing time
-    System.out.println("Restocking inventory for order: " + orderId);
-    // Implement the actual inventory restocking logic here
-  }
-
-  @Override
-  public void cancelDelivery(String orderId) {
-    sleep(5); // simulate API call processing time
-    System.out.println("Reverting order: " + orderId);
-    // Implement the actual order reversion logic here
-  }
-
-  private void sleep(int seconds) {
-    try {
-      // a random number between 800 and 1200
-      // to simulate variance in API call time
-      long sleepTime = (long) (Math.random() * 400) + 800;
-
-      Thread.sleep(seconds * sleepTime);
-    } catch (InterruptedException e) {
-      Thread.currentThread().interrupt();
+    @Override
+    public void processPayment(String orderId) {
+        System.out.println("Processing payment for order: " + orderId);
+        sleep(5); // simulate API call processing time
+        // Implement the actual payment processing logic here
     }
-  }
+
+    @Override
+    public void reserveInventory(String orderId) {
+        System.out.println("Reserving inventory for order: " + orderId);
+        sleep(5); // simulate API call processing time
+        // Implement the actual inventory reservation logic here
+
+        // Simulate inventory reservation failure
+        //    Exception e = new RuntimeException("Inventory reservation failed");
+        //    throw ApplicationFailure.newNonRetryableFailure(e.getMessage(), e.getClass().getName());
+    }
+
+    @Override
+    public void deliverOrder(String orderId) {
+        System.out.println("Delivering order: " + orderId);
+        sleep(5); // simulate API call processing time
+        // Implement the actual order delivery logic here
+
+        // Simulate delivery failure
+        //    Exception e = new RuntimeException("Delivery failed");
+        //    throw ApplicationFailure.newNonRetryableFailure(e.getMessage(), e.getClass().getName());
+    }
+
+    @Override
+    public void refundPayment(String orderId) {
+        sleep(5); // simulate API call processing time
+        System.out.println("Refunding payment for order: " + orderId);
+        // Implement the actual payment refund logic here
+    }
+
+    @Override
+    public void restockInventory(String orderId) {
+        sleep(5); // simulate API call processing time
+        System.out.println("Restocking inventory for order: " + orderId);
+        // Implement the actual inventory restocking logic here
+    }
+
+    @Override
+    public void cancelDelivery(String orderId) {
+        sleep(5); // simulate API call processing time
+        System.out.println("Reverting order: " + orderId);
+        // Implement the actual order reversion logic here
+    }
+
+    private void sleep(int seconds) {
+        try {
+            // a random number between 800 and 1200
+            // to simulate variance in API call time
+            long sleepTime = (long) (Math.random() * 400) + 800;
+
+            Thread.sleep(seconds * sleepTime);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
 }
